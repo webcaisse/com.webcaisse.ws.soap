d27fd3e6d95e281289724262c4c7ac83139a6c46
Khaled <sghaierkhaled@live.com>
2015-01-01 23:37:24 +0100

    ajout d'autre champs au formulaire

--- src/main/java/com/webcaisse/ws/impl/ClientManagerServiceImpl.java
+++ src/main/java/com/webcaisse/ws/impl/ClientManagerServiceImpl.java
@@ -31,6 +31,18 @@ public class ClientManagerServiceImpl implements ClientManagerService {
 			c.setPrenom(client.getPrenom());
 			c.setTelephone(client.getTelephone());
 			c.setId(client.getId());
+			c.setCode1(client.getCode1());
+			c.setCode2(client.getCode2());
+			c.setCode3(client.getCode3());
+			c.setCodePostale(client.getCodePostale());
+			c.setEtage(client.getEtage());
+			c.setImmeuble(client.getImmeuble());
+			c.setInterphone(client.getInterphone());
+			c.setNumeroRue(client.getNumeroRue());
+			c.setNomRue(client.getNomVoie());
+			c.setVille(client.getVille());
+			
+			
 			clientVo.add(c);
 		}
 
@@ -47,6 +59,17 @@ public class ClientManagerServiceImpl implements ClientManagerService {
 		client.setEmail(c.getEmail());
 		client.setTelephone(c.getTelephone());
 		client.setSociete(societe);
+		client.setCode1(c.getCode1());
+		client.setCode2(c.getCode2());
+		client.setCode3(c.getCode3());
+		client.setCodePostale(c.getCodePostale());
+		client.setEtage(c.getEtage());
+		client.setImmeuble(c.getImmeuble());
+		client.setInterphone(c.getInterphone());
+		client.setNumeroRue(c.getNumeroRue());
+		client.setNomVoie(c.getNomRue());
+		client.setVille(c.getVille());
+		
 		
 		clientDao.ajouterClient(client) ;
 		
@@ -65,6 +88,17 @@ public class ClientManagerServiceImpl implements ClientManagerService {
 		client.setPrenom(c.getPrenom());
 		client.setEmail(c.getEmail());
 		client.setTelephone(c.getTelephone());
+		client.setCode1(c.getCode1());
+		client.setCode2(c.getCode2());
+		client.setCode3(c.getCode3());
+		client.setCodePostale(c.getCodePostale());
+		client.setEtage(c.getEtage());
+		client.setImmeuble(c.getImmeuble());
+		client.setInterphone(c.getInterphone());
+		client.setNumeroRue(c.getNumeroRue());
+		client.setNomVoie(c.getNomRue());
+		client.setVille(c.getVille());
+		
 		clientDao.updateClient(client);
 		
 	}
@@ -82,6 +116,17 @@ public class ClientManagerServiceImpl implements ClientManagerService {
 		clientVo.setPrenom(client.getPrenom());
 		clientVo.setEmail(client.getEmail());
 		clientVo.setTelephone(client.getTelephone());
+		clientVo.setCode1(client.getCode1());
+		clientVo.setCode2(client.getCode2());
+		clientVo.setCode3(client.getCode3());
+		clientVo.setCodePostale(client.getCodePostale());
+		clientVo.setEtage(client.getEtage());
+		clientVo.setImmeuble(client.getImmeuble());
+		clientVo.setInterphone(client.getInterphone());
+		clientVo.setNumeroRue(client.getNumeroRue());
+		clientVo.setNomRue(client.getNomVoie());
+		clientVo.setVille(client.getVille());
+		
 					
 		}
 		
