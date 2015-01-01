479363d8dbb9b5941d734de764d65ba1f5106c04
Khaled <sghaierkhaled@live.com>
2015-01-01 20:10:46 +0100

    dev MAJ Client

--- src/main/java/com/webcaisse/ws/impl/ClientManagerServiceImpl.java
+++ src/main/java/com/webcaisse/ws/impl/ClientManagerServiceImpl.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.webcaisse.dao.hibernate.IClient;
 import com.webcaisse.dao.hibernate.model.Client;
+import com.webcaisse.dao.hibernate.model.Societe;
 import com.webcaisse.ws.interfaces.ClientManagerService;
+import com.webcaisse.ws.model.ClientIn;
 import com.webcaisse.ws.model.ClientOut;
 
 public class ClientManagerServiceImpl implements ClientManagerService {
@@ -20,17 +22,69 @@ public class ClientManagerServiceImpl implements ClientManagerService {
 		List<ClientOut> clientVo = new ArrayList<ClientOut>();
 
 		List<Client> clients = clientDao.rechercherClient(idSociete);
-		ClientOut c = new ClientOut();
+		
 
 		for (Client client : clients) {
-
+			ClientOut c = new ClientOut();
 			c.setEmail(client.getEmail());
 			c.setNom(client.getNom());
 			c.setPrenom(client.getPrenom());
 			c.setTelephone(client.getTelephone());
+			c.setId(client.getId());
 			clientVo.add(c);
 		}
 
+		return clientVo;
+	}
+
+	public void ajouterClient(ClientIn c) {
+	
+		Client client = new Client() ;
+		Societe societe = new Societe() ;
+		societe.setId(c.getIdSociete());
+		client.setNom(c.getNom());
+		client.setPrenom(c.getPrenom());
+		client.setEmail(c.getEmail());
+		client.setTelephone(c.getTelephone());
+		client.setSociete(societe);
+		
+		clientDao.ajouterClient(client) ;
+		
+	}
+
+	public void supprimerClient(Long idClient) {
+		
+		clientDao.supprimerClient(idClient);
+	}
+
+	public void updateClient(ClientIn c) {
+		
+		Client client = clientDao.loadClientById(c.getId()) ;
+		
+		client.setNom(c.getNom());
+		client.setPrenom(c.getPrenom());
+		client.setEmail(c.getEmail());
+		client.setTelephone(c.getTelephone());
+		clientDao.updateClient(client);
+		
+	}
+
+	public ClientOut loadClientById(Long IdClient) {
+		
+		
+		ClientOut  clientVo = null ;
+		
+		Client client = clientDao.loadClientById(IdClient) ;
+		if (client != null){
+			
+			clientVo=new ClientOut() ;
+		clientVo.setNom(client.getNom());
+		clientVo.setPrenom(client.getPrenom());
+		clientVo.setEmail(client.getEmail());
+		clientVo.setTelephone(client.getTelephone());
+					
+		}
+		
 		return clientVo;
 	}
 
