5f15172def6b3cfc5a2296f1ce6292257a3e5e3b
abdessalem <belhadj.abdessalem@gmail.com>
2015-01-03 23:13:45 +0100

    dev parametrage caisse

--- src/main/java/com/webcaisse/ws/impl/ParametreManagerServiceImpl.java
+++ src/main/java/com/webcaisse/ws/impl/ParametreManagerServiceImpl.java
@@ -1,6 +1,7 @@
 package com.webcaisse.ws.impl;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,29 +17,28 @@ public class ParametreManagerServiceImpl implements ParametreManagerService {
 
 	@Autowired
 	IParametreDao parametreDao;
-	
+
 	@Autowired
-	ISocieteDao societeDao ;
-	
+	ISocieteDao societeDao;
+
 	public void sauvegarderParametre(ParametreIn parametre) {
-		
-	
-			Reference reference = new Reference () ; 
-		
-			Societe societe = societeDao.loadById(parametre.getIdSociete());
-			
-			//reference.setDateCrea(parametre.getDateCrea());
-			//reference.setDateModif(parametre.getDateModif());
+
+		// je verifie si ce parametre existe deja dans la base
+		Reference reference = parametreDao.getReferenceByName(parametre
+				.getNomParametre());
+		if (reference == null) {
+			// création
+			reference = new Reference();
+			reference.setDateCrea(new Date());
 			reference.setNomParametre(parametre.getNomParametre());
-			//reference.setValeur(parametre.getValeur());
-			reference.setSociete(societe);
-			
-			parametreDao.sauvgarderParametre(reference);
-			
-		
-		
-			
-		
+			reference.setValeur(parametre.getValeur());
+			reference.setSociete(societeDao.loadById(parametre.getIdSociete()));
+		} else {
+			// modification
+			reference.setValeur(parametre.getValeur());
+		}
+		parametreDao.sauvgarderParametre(reference);
+
 	}
 
 }
