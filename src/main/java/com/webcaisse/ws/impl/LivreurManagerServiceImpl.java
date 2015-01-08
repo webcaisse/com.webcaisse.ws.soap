37b6003982f49faf67277be4f737197327e76f4c
Khaled <sghaierkhaled@live.com>
2015-01-08 20:34:13 +0100

    dev param livreur

---
+++ src/main/java/com/webcaisse/ws/impl/LivreurManagerServiceImpl.java
@@ -0,0 +1,59 @@
+package com.webcaisse.ws.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.webcaisse.dao.hibernate.ILivreurDao;
+import com.webcaisse.dao.hibernate.model.Livreur;
+import com.webcaisse.dao.hibernate.model.Societe;
+import com.webcaisse.dao.hibernate.model.User;
+import com.webcaisse.ws.interfaces.LivreurManagerService;
+import com.webcaisse.ws.model.LivreurIn;
+import com.webcaisse.ws.model.LivreurOut;
+import com.webcaisse.ws.model.UserOut;
+
+public class LivreurManagerServiceImpl implements LivreurManagerService {
+
+	
+	
+	@Autowired
+	ILivreurDao livreurDao ;
+	
+	public void sauvegarderLivreur(LivreurIn livreur) {
+		Livreur l = new Livreur() ;
+		Societe societe = new Societe() ;
+		societe.setId(livreur.getSocieteId());
+		l.setNom(livreur.getNom());
+		l.setPrenom(livreur.getPrenom());
+		l.setTelephone(livreur.getTelephone());
+		l.setAdresse(livreur.getAdresse());
+		l.setNss(livreur.getNss());
+		l.setSociete(societe);
+		
+		livreurDao.sauvegarderLivreur(l);
+		
+	}
+
+	public List<LivreurOut> rechercherLivreur(Long idSociete) {
+		
+		List<LivreurOut> livreurVo = new ArrayList<LivreurOut>();
+		List<Livreur> livreurs = livreurDao.rechercherLivreur(idSociete) ;
+		
+		for (Livreur livreur : livreurs) {
+			LivreurOut  livreurOut = new LivreurOut() ;
+			
+			livreurOut.setNom(livreur.getNom());
+			livreurOut.setPrenom(livreur.getPrenom());
+			livreurOut.setAdresse(livreur.getAdresse());
+			
+			livreurVo.add(livreurOut) ;
+		}
+		
+		
+		return livreurVo;
+	}
+	}
+
+
